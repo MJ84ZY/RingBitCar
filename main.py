e8f326cab16dabc1dd2f4fdbf3acae883a4b8979
ZYing <5281999zhiying@gmail.com>
2024-01-10 11:28:08 +0800

    Update ALL

--- main.py
+++ main.py
@@ -1,57 +1,101 @@
-devices.onGamepadButton(MesDpadButtonInfo._4Down, function () {
+def on_gamepad_button__4_down():
+    InitDirection()
+    basic.show_arrow(ArrowNames.WEST)
     RingbitCar.turnright()
-    basic.showString("4")
-})
-devices.onGamepadButton(MesDpadButtonInfo._3Down, function () {
+devices.on_gamepad_button(MesDpadButtonInfo._4_DOWN, on_gamepad_button__4_down)
+
+def Testing():
+    display.rotate_to(display.Direction.LOGO_TO_RIGHT)
+    basic.show_icon(IconNames.HAPPY)
+
+def on_gamepad_button__3_down():
+    InitDirection()
+    basic.show_arrow(ArrowNames.EAST)
     RingbitCar.turnleft()
-    basic.showString("3")
-})
-bluetooth.onBluetoothConnected(function () {
-    basic.showIcon(IconNames.Scissors)
-    music.startMelody(music.builtInMelody(Melodies.Ringtone), MelodyOptions.Once)
-    basic.pause(200)
-})
-bluetooth.onBluetoothDisconnected(function () {
-    basic.showIcon(IconNames.Sad)
-    strip.showColor(neopixel.colors(NeoPixelColors.Red))
+devices.on_gamepad_button(MesDpadButtonInfo._3_DOWN, on_gamepad_button__3_down)
+
+def on_bluetooth_connected():
+    global bBlueConnected
+    basic.show_icon(IconNames.SCISSORS)
+    bBlueConnected = 1
+    strip.show_color(neopixel.colors(NeoPixelColors.GREEN))
+    if bSoundOn:
+        music.start_melody(music.built_in_melody(Melodies.RINGTONE), MelodyOptions.ONCE)
+        basic.pause(200)
+bluetooth.on_bluetooth_connected(on_bluetooth_connected)
+
+def on_bluetooth_disconnected():
+    global bBlueConnected
+    basic.show_icon(IconNames.SAD)
+    bBlueConnected = 0
+    strip.show_color(neopixel.colors(NeoPixelColors.RED))
     basic.pause(100)
-    strip.showColor(neopixel.colors(NeoPixelColors.Orange))
-    basic.pause(100)
-})
-devices.onGamepadButton(MesDpadButtonInfo.BDown, function () {
-    basic.showString("B")
+bluetooth.on_bluetooth_disconnected(on_bluetooth_disconnected)
+
+def on_button_pressed_a():
+    display.rotate_to(display.Direction.UPSIDE_DOWN)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_gamepad_button__2_down():
+    InitDirection()
+    basic.show_arrow(ArrowNames.SOUTH)
     RingbitCar.back()
-})
-input.onButtonPressed(Button.A, function () {
-    RingbitCar.forward()
-    basic.pause(200)
+devices.on_gamepad_button(MesDpadButtonInfo._2_DOWN, on_gamepad_button__2_down)
+
+def on_gamepad_button_cdown():
     RingbitCar.brake()
-})
-devices.onGamepadButton(MesDpadButtonInfo._2Down, function () {
-    basic.showString("2")
-})
-devices.onGamepadButton(MesDpadButtonInfo.DDown, function () {
-    basic.showString("D")
-})
-devices.onGamepadButton(MesDpadButtonInfo.CDown, function () {
-    RingbitCar.brake()
-    basic.showString("C")
-})
-devices.onGamepadButton(MesDpadButtonInfo.ADown, function () {
-    basic.showString("A")
+devices.on_gamepad_button(MesDpadButtonInfo.CDOWN, on_gamepad_button_cdown)
+
+def InitDirection():
+    global bTop, bBottom, bLeft, bRight
+    bTop = 0
+    bBottom = 0
+    bLeft = 0
+    bRight = 0
+
+def on_gamepad_button_adown():
+    pass
+devices.on_gamepad_button(MesDpadButtonInfo.ADOWN, on_gamepad_button_adown)
+
+def Main():
+    global strip
+    music.set_built_in_speaker_enabled(True)
+    InitInstance()
+    basic.show_icon(IconNames.HAPPY)
+    RingbitCar.init_wheel(AnalogPin.P1, AnalogPin.P2)
+    strip = neopixel.create(DigitalPin.P0, 2, NeoPixelMode.RGB)
+    strip.show_color(neopixel.colors(NeoPixelColors.VIOLET))
+
+def on_gamepad_button__1_down():
+    InitDirection()
+    basic.show_arrow(ArrowNames.SOUTH)
     RingbitCar.forward()
-})
-devices.onGamepadButton(MesDpadButtonInfo._1Down, function () {
-    basic.showString("1")
-})
-input.onLogoEvent(TouchButtonEvent.Touched, function () {
-    basic.showIcon(IconNames.Butterfly)
-})
-let strip: neopixel.Strip = null
-music.setBuiltInSpeakerEnabled(true)
-basic.showNumber(0)
-RingbitCar.init_wheel(AnalogPin.P1, AnalogPin.P2)
-strip = neopixel.create(DigitalPin.P0, 2, NeoPixelMode.RGB)
-basic.forever(function () {
-	
-})
+devices.on_gamepad_button(MesDpadButtonInfo._1_DOWN, on_gamepad_button__1_down)
+
+def on_logo_touched():
+    basic.show_icon(IconNames.BUTTERFLY)
+input.on_logo_event(TouchButtonEvent.TOUCHED, on_logo_touched)
+
+def InitInstance():
+    global bBlueConnected, bSoundOn
+    bBlueConnected = 0
+    bSoundOn = 0
+    InitDirection()
+def ClearLed():
+    pass
+bRight = 0
+bLeft = 0
+bBottom = 0
+bTop = 0
+bSoundOn = 0
+strip: neopixel.Strip = None
+bBlueConnected = 0
+Testing()
+
+def on_every_interval():
+    pass
+loops.every_interval(500, on_every_interval)
+
+def on_forever():
+    pass
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,18 +1,20 @@
-devices.onGamepadButton(MesDpadButtonInfo._4Down, function () {
+devices.onGamepadButton(MesDpadButtonInfo._4Down, function on_gamepad_button__4_down() {
     InitDirection()
     basic.showArrow(ArrowNames.West)
     RingbitCar.turnright()
 })
-function Testing () {
+function Testing() {
     display.rotateTo(display.Direction.LogoToRight)
     basic.showIcon(IconNames.Happy)
 }
-devices.onGamepadButton(MesDpadButtonInfo._3Down, function () {
+
+devices.onGamepadButton(MesDpadButtonInfo._3Down, function on_gamepad_button__3_down() {
     InitDirection()
     basic.showArrow(ArrowNames.East)
     RingbitCar.turnleft()
 })
-bluetooth.onBluetoothConnected(function () {
+bluetooth.onBluetoothConnected(function on_bluetooth_connected() {
+    
     basic.showIcon(IconNames.Scissors)
     bBlueConnected = 1
     strip.showColor(neopixel.colors(NeoPixelColors.Green))
@@ -20,34 +22,39 @@ bluetooth.onBluetoothConnected(function () {
         music.startMelody(music.builtInMelody(Melodies.Ringtone), MelodyOptions.Once)
         basic.pause(200)
     }
+    
 })
-bluetooth.onBluetoothDisconnected(function () {
+bluetooth.onBluetoothDisconnected(function on_bluetooth_disconnected() {
+    
     basic.showIcon(IconNames.Sad)
     bBlueConnected = 0
     strip.showColor(neopixel.colors(NeoPixelColors.Red))
     basic.pause(100)
 })
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     display.rotateTo(display.Direction.UpsideDown)
 })
-devices.onGamepadButton(MesDpadButtonInfo._2Down, function () {
+devices.onGamepadButton(MesDpadButtonInfo._2Down, function on_gamepad_button__2_down() {
     InitDirection()
     basic.showArrow(ArrowNames.South)
     RingbitCar.back()
 })
-devices.onGamepadButton(MesDpadButtonInfo.CDown, function () {
+devices.onGamepadButton(MesDpadButtonInfo.CDown, function on_gamepad_button_cdown() {
     RingbitCar.brake()
 })
-function InitDirection () {
+function InitDirection() {
+    
     bTop = 0
     bBottom = 0
     bLeft = 0
     bRight = 0
 }
-devices.onGamepadButton(MesDpadButtonInfo.ADown, function () {
-	
+
+devices.onGamepadButton(MesDpadButtonInfo.ADown, function on_gamepad_button_adown() {
+    
 })
-function Main () {
+function Main() {
+    
     music.setBuiltInSpeakerEnabled(true)
     InitInstance()
     basic.showIcon(IconNames.Happy)
@@ -55,33 +62,37 @@ function Main () {
     strip = neopixel.create(DigitalPin.P0, 2, NeoPixelMode.RGB)
     strip.showColor(neopixel.colors(NeoPixelColors.Violet))
 }
-devices.onGamepadButton(MesDpadButtonInfo._1Down, function () {
+
+devices.onGamepadButton(MesDpadButtonInfo._1Down, function on_gamepad_button__1_down() {
     InitDirection()
     basic.showArrow(ArrowNames.South)
     RingbitCar.forward()
 })
-input.onLogoEvent(TouchButtonEvent.Touched, function () {
+input.onLogoEvent(TouchButtonEvent.Touched, function on_logo_touched() {
     basic.showIcon(IconNames.Butterfly)
 })
-function InitInstance () {
+function InitInstance() {
+    
     bBlueConnected = 0
     bSoundOn = 0
     InitDirection()
 }
-function ClearLed () {
-	
+
+function ClearLed() {
+    
 }
+
 let bRight = 0
 let bLeft = 0
 let bBottom = 0
 let bTop = 0
 let bSoundOn = 0
-let strip: neopixel.Strip = null
+let strip : neopixel.Strip = null
 let bBlueConnected = 0
 Testing()
-loops.everyInterval(500, function () {
-	
+loops.everyInterval(500, function on_every_interval() {
+    
 })
-basic.forever(function () {
-	
+basic.forever(function on_forever() {
+    
 })
